bd9f528e35b6852fd33bc89d45099b64d7bb9e91
Juan-Pablo-Barrientos <assacreed100@gmail.com>
2021-08-18 20:07:26 -0300

    servlet añadido 2

---
+++ Java Proyecto 123/src/main/java/servlet/Signin.java
@@ -0,0 +1,47 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Signin
+ */
+@WebServlet({ "/Signin", "/signin", "/SIGNIN" })
+public class Signin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Signin() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("get at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String email = request.getParameter("InputEmail");
+		String password = request.getParameter("InputPass");
+		response.getWriter()
+		.append("Email: ")
+		.append(email)
+		.append("Pass: ")
+		.append(password)
+		.append(" post at: ").append(request.getContextPath());
+	}
+
+}
