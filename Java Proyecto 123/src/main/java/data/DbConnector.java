b083ec5063ab7cfb8f6a2e21dc0317e9291fe59e
tomicervillera <cervilleratomas@outlook.com>
2021-09-20 17:20:59 -0300

    Agregada excepciones a DbConnector.

--- Java Proyecto 123/src/main/java/data/DbConnector.java
+++ Java Proyecto 123/src/main/java/data/DbConnector.java
@@ -2,56 +2,75 @@ package data;
 
 import java.sql.*;
 
-public class DbConnector {
+public class DbConnector
+{
 
-	private static DbConnector instancia;
-	
-	private String driver="com.mysql.cj.jdbc.Driver";
-	private String host="localhost";
-	private String port="3306";
-	private String user="java";
-	private String password="himitsu";
-	private String db="market_tp";
-	private int conectados=0;
-	private Connection conn=null;
-	
-	private DbConnector() {
-		try {
-			Class.forName(driver);
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
+    private static DbConnector instancia;
+
+    private String driver = "com.mysql.cj.jdbc.Driver";
+    private String host = "localhost";
+    private String port = "3306";
+    private String user = "java";
+    private String password = "himitsu";
+    private String db = "market_tp";
+    private int conectados = 0;
+    private Connection conn = null;
+
+    private DbConnector()
+    {
+	try
+	{
+	    Class.forName(driver);
 	}
-	
-	public static DbConnector getInstancia() {
-		if (instancia == null) {
-			instancia = new DbConnector();
-		}
-		return instancia;
+	catch (ClassNotFoundException e)
+	{
+	    e.printStackTrace();
 	}
-	
-	public Connection getConn() {
-		try {
-			if(conn==null || conn.isClosed()) {
-				conn=DriverManager.getConnection("jdbc:mysql://"+host+":"+port+"/"+db, user, password);
-				conectados=0;
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		conectados++;
-		return conn;
+    }
+
+    public static DbConnector getInstancia()
+    {
+	if (instancia == null)
+	{
+	    instancia = new DbConnector();
 	}
-	
-	public void releaseConn() {
-		conectados--;
-		try {
-			if (conectados<=0) {
-				conn.close();
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+	return instancia;
+    }
+
+    public Connection getConn() throws SQLException
+    {
+	try
+	{
+	    if (conn == null || conn.isClosed())
+	    {
+		conn = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" + db, user, password);
+		conectados = 0;
+	    }
 	}
+	catch (SQLException e)
+	{
+	    e.printStackTrace();
+	    throw e;
+	}
+	conectados++;
+	return conn;
+    }
+
+    public void releaseConn() throws SQLException
+    {
+	conectados--;
+	try
+	{
+	    if (conectados <= 0)
+	    {
+		conn.close();
+	    }
+	}
+	catch (SQLException e)
+	{
+	    e.printStackTrace();
+	    throw e;
+	}
+    }
 
 }
